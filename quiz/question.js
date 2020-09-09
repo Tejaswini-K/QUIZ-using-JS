5de8624d4375fbfdba38af8ae1b949b977180b5f
Tejaswini <58681247+Tejaswini-K@users.noreply.github.com>
2020-09-09 19:10:45 +0530

    Add files via upload

---
+++ quiz/question.js
@@ -0,0 +1,29 @@
+var questions = [{
+    "question":"The first mechanical computer designed by Charles abbage was called ?",
+     "option1":"Abacus",
+     "option2":"Analytical Engine",
+     "option3":"Calculator",
+     "option4":"Processor",
+      "answer":"2"
+}, {
+    "question":"The language spoken by the people by Pakistan is ?",
+     "option1":"Hindi",
+     "option2":"Palauan",
+     "option3":"Sindhi",
+     "option4":"Nauruan",
+      "answer":"3"
+},{
+    "question":"The World Largest desert is ?",
+      "option1":"Thar",
+     "option2":"Kalahari",
+     "option3":"Sonoran",
+     "option4":"Sahara",
+      "answer":"4"
+},{
+    "question":" Country that has the highest in Barley Production ?",
+     "option1":"Russia",
+     "option2":"India",
+     "option3":"China",
+     "option4":"France",
+      "answer":"1"
+}]
